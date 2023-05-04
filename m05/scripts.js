386df30929bea26c04405501dbf765cb98685795
miaS057 <90110809+miaS057@users.noreply.github.com>
2023-05-03 19:01:32 -0700

    Update scripts.js

--- m05/scripts.js
+++ m05/scripts.js
@@ -1,21 +1,10 @@
-var table = prompt('Enter a number from 1-10');             // Unit of table
-var operator = 'multiplication';                            // Type of calculation
+var table = prompt('Enter a number from 1-10');             // Unit of table           
 var i = 1;                                                  // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';                  // Message
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} 
-else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+while (i < 11) {
+  msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+  i++;
 }
 
 // Write the message into the page
