13d01e6a230e8581a647aba6fe39b019096f1bd7
miaS057 <90110809+miaS057@users.noreply.github.com>
2023-06-05 15:13:02 -0700

    Update scripts.js

--- final/scripts.js
+++ final/scripts.js
@@ -1,7 +1,10 @@
 function validateForm() {
+  //Create variables
   var errorMessage = "";
-  var validFirstname = false;
-  var firstname = document.getElementById("FirstName").value;
+
+  var validName = false;
+  var name = document.getElementById("Name").value;
+
 
   var validEmail = false;
   var email = document.getElementById("Email").value;
@@ -9,45 +12,38 @@ function validateForm() {
   var dotpos = email.lastIndexOf(".");
 
   var validComment = false;
-  var comment = document.getElementById("Comment").value;
+  var comment = document.getElementById("Comments").value;
 
-  //Validate first name  
-  if (firstname === "null" || firstname === "" || firstname.length > 20 || onlyLetters(firstname)) {
-    errorMessage = "<p>The first name is required and cannot be greater than 20 characters</p>";
-    errorMessage += "<p>Only alphbetical characters are accepted<p>";
+
+
+  //Validate name  
+  if (name === "null" || name === "") {
+    errorMessage = "<p>*Invalid name*<p>";
   } else {
     validFirstname = true;
     errorMessage = "";
   }
-  document.getElementById("firstNameErrorMsg").innerHTML = errorMessage;
+  document.getElementById("nameErrorMsg").innerHTML = errorMessage;
+
 
   //Validate email
   if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length || email === "null" || email === "") {
-    errorMessage = "<p>Invalid email</p>";
+    errorMessage = "<p>*Invalid email*</p>";
   } else {
     validEmail = true;
     errorMessage = "";
   }
   document.getElementById("emailErrorMsg").innerHTML = errorMessage;
-
-  //Validate comment
+  
+  //Validate comments  
   if (comment === "null" || comment === "") {
-    errorMessage = "<p>The comment section is required</p>";
+    errorMessage = "<p>*This field is required*<p>";
   } else {
-    validFirstname = true;
+    validComment = true;
     errorMessage = "";
   }
   document.getElementById("commentErrorMsg").innerHTML = errorMessage;
-  
-  return (validFirstname && validEmail && validComment);
-}
 
 
-function onlyLetters(inputString) {
-  var letters = /^[A-Za-z]+$/;
-  if (inputString.value.match(letters)) {
-    return true;
-  } else {
-    return false;
-  }
+  return (validName && validEmail && validComment);
 }
